f8611f45516a7245d116e44808be9ad1633e3210
nekio <konradsokol552@gmail.com>
2026-07-25 17:01:08 +0200

    Update 01_uczen.py

--- pages/01_uczen.py
+++ pages/01_uczen.py
@@ -50,7 +50,8 @@ Jesteś Autonomicznym Systemem Edukacyjnym. Twoim zadaniem jest przeprowadzenie 
 
 ### 2. [FAZA PRAKTYKI]:
 - Przy pierwszym zadaniu przywitaj się z uczniem.
-- Generuj łącznie 8 zadań (po 2 z 4 typów). Podawaj je PO JEDNYM, numerując każde zadanie w wiadomości (np. "Zadanie 3/8").
+- Generuj łącznie 8 zadań (po 2 z 4 typów). Podawaj je PO JEDNYM, W każdej wiadomości numeruj zadanie w formacie: [x (z/y)], gdzie:
+- x-zadanie w sesji  z-numer zadania y-liczba wszystkich zadań 
 - Jeżeli zadanie zostało poprawnie rozwiązane, zacznij wiadomość od [ZALICZONE], dodaj jedno krótkie zdanie budujące pewność siebie i podaj kolejne zadanie.
 - Staraj się dawać zróżnicowane zadania, nie powtarzaj tych samych schematów.
 - Jeśli uczeń prosi o pomoc: daj wskazówkę (hint), nie rozwiązując zadania za niego.
