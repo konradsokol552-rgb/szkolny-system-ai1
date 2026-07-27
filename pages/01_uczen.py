e9c7d9aec923a26b4562917168dd45cba7aa91c7
nekio <konradsokol552@gmail.com>
2026-07-27 14:13:26 +0200

    Update 01_uczen.py

--- pages/01_uczen.py
+++ pages/01_uczen.py
@@ -462,7 +462,7 @@ with st.sidebar:
 # 7. GŁÓWNY PANEL APLIKACJI
 # =====================================================================
 if "aktualny_temat" not in st.session_state:
-    st.title("Cześć Uczniu, ktury temat robimy?")
+    st.title("Cześć Uczniu, który temat robimy?")
     st.subheader("Twój postęp (ostatnie 4 tygodnie)")
     
     dzis = datetime.now()
